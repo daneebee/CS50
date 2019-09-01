36f14c9bec20c24ff6705b432662c34cdb2acc36
Daniel Bodmer <danny.bodmer@gmail.com>
2019-09-01 10:41:33 +0100

    Update Collatz sequence to show total steps

--- collatz.c
+++ collatz.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
 int collatz(int n);
-int counter = 0;
 
 int main(void) {
   int n = 7;
@@ -15,13 +14,9 @@ int collatz(int n) {
   printf("n is currently %i\n", n);
 
   if (n == 1)
-    return 1;
-  else if (n % 2 == 0) {
-    counter++;
-    return collatz(n / 2);
-  }
-  else {
-    counter++;
-    return collatz((3 * n) + 1);
-  }
+    return 0;
+  else if (n % 2 == 0)
+    return 1 + collatz(n / 2);
+  else 
+    return 1 + collatz((3 * n) + 1);
 }
