6f712b5489f850272148341abdf6c01462a46b31
Daniel Bodmer <danny.bodmer@gmail.com>
2019-09-22 14:09:35 +0100

    Complete week 7

--- week_7/survey/application.py
+++ week_7/survey/application.py
@@ -31,9 +31,42 @@ def get_form():
 
 @app.route("/form", methods=["POST"])
 def post_form():
-    return render_template("error.html", message="TODO")
+    form_values = {}
+    missing_values = []
+
+    form_values['first_name'] = request.form["firstname"]
+    form_values['last_name'] = request.form["lastname"]
+    form_values['favorite_color'] = request.form.get("color")
+    form_values['favorite_programming'] = request.form.get("programming")
+    form_values['comment'] = request.form["comment"]
+
+    for key, value in form_values.items():
+        if value is None or len(value) == 0:
+            missing_values.append(key)
+
+    if len(missing_values) > 0:
+        missing_values = [x.replace('_', " ") for x in missing_values]
+        error_message = "Please provided the following missing values: {}".format(', '.join(missing_values))
+        return render_template("error.html", message=error_message)
+
+    csv_fieldnames = ['firstname', 'lastname', 'color', 'programming', 'comment']
+
+    with open('survey.csv', 'a') as file:
+        writer = csv.DictWriter(file, fieldnames=csv_fieldnames)
+        writer.writerow({'firstname': form_values['first_name'],
+                         'lastname': form_values['last_name'],
+                         'color': form_values['favorite_color'],
+                         'programming': form_values['favorite_programming'],
+                         'comment': form_values['comment']})
+    return redirect("/sheet")
 
 
 @app.route("/sheet", methods=["GET"])
 def get_sheet():
-    return render_template("error.html", message="TODO")
+    rows = {}
+    with open("survey.csv", "r") as file:
+        reader = csv.reader(file)
+        rows = list(reader)
+        print(rows)
+        return render_template("sheet.html", rows=rows)
+    return render_template("error.html", message="Error extracting CSV data")
